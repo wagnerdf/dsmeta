8bf0bf33b99003e9b4164b91bdd29787d30f59df
wagner.augusto <wagner.augusto@tecnisys.com.br>
2022-11-18 21:31:39 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 
-import Header from "./components/Header"
+import Header from "./components/Header";
 
-import SalesCard from "./components/SalesCard"
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return(
@@ -19,4 +19,4 @@ function App() {
 
 }
 
-export default App
+export default App;
